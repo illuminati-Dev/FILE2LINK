b94f35225732e2b7c7b06decb66303d9754889c0
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-10-29 20:58:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgresql://fabzskun:sEZuaHg0PEju6saN7pPQAUgcHIkEiVPm@flora.db.elephantsql.com/fabzskun")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001209619698"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0")) 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
